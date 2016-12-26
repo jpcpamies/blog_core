177431cc8986531857fca8cef5f82f884d055bc8
Jordi Pàmies <jordipamies2@gmail.com>
2016-12-26 23:26:30 +0000

    Asignando al visitis_count el valor 0 para nuevo artículos desde el modelo. Antes quedaba como nil

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,12 @@ class Article < ApplicationRecord
 	belongs_to :user
 	validates :title, presence: true, uniqueness: true
 	validates :body, presence: true, length: { minimum: 20 }
+	# Esto setea la cuenta de visitas a 0. Lo hacemos con un before_ ya que no lo seteamos en la bbdd
+	before_create :set_visits_count
+
+	private
+
+	def set_visits_count
+		self.visits_count = 0
+	end
 end
