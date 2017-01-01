7fb554beaba4659184a4a0c448440806e6260f6a
Jordi Pàmies <jordipamies2@gmail.com>
2017-01-01 02:50:57 +0000

    Eliminadas tablas categories y has_categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170101020842) do
+ActiveRecord::Schema.define(version: 20170101024731) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -24,22 +24,6 @@ ActiveRecord::Schema.define(version: 20170101020842) do
     t.integer  "cover_file_size"
     t.datetime "cover_updated_at"
     t.index ["user_id"], name: "index_articles_on_user_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.string   "color"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "has_categories", force: :cascade do |t|
-    t.integer  "article_id"
-    t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["article_id"], name: "index_has_categories_on_article_id"
-    t.index ["category_id"], name: "index_has_categories_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
