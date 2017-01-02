5bc9448e8dea291db061fff336a72c0f8e8d827e
Jordi Pàmies <jordipamies2@gmail.com>
2017-01-02 18:18:15 +0000

    Añadido callback en el article controller para restringir acciones a determinados tipos de usuario. Solo puede crear y actualizar el editor, y eliminar el administrador, el normal solo puede ver.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_categories
 
-  # protected
+  protected
 
-  # 	def authenticate_editor!
-  # 		redirect_to root_path unless user_signed_in? && current_user.is_editor?
-  # 	end
+  	def authenticate_editor!
+  		redirect_to root_path unless user_signed_in? && current_user.is_editor?
+  	end
 
-  # 	def authenticate_admin!
-  # 		redirect_to root_path unless user_signed_in? && current_user.is_admin?
-  # 	end
+  	def authenticate_admin!
+  		redirect_to root_path unless user_signed_in? && current_user.is_admin?
+  	end
 
   private
 
