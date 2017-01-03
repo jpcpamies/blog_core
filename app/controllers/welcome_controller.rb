fddb9ef587e610cf1b608993d3e90e7fec53b327
Jordi Pàmies <jordipamies2@gmail.com>
2017-01-03 11:36:14 +0000

    Desplegando todos los artículos en /dashboard#index con un before acction para solo acceso por el admin

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,10 @@
 class WelcomeController < ApplicationController
+	# Esto dice que solo el admin puede acceder al dashboard
+	before_action :authenticate_admin!, only: [:dashboard]
   def index
   end
 
   def dashboard
-  	
+  	@articles = Article.all
   end
 end
