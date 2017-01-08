09ef02a3c85c4b5689b17d6f43d6d94851741032
Jordi Pàmies <jordipamies2@gmail.com>
2017-01-08 13:33:07 +0000

    Creado helper para encapsular el header de la vista article#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	# Le paso dos parámetros para poder hacer que sea dinámico y usarlo por separado en cada vista independientemente.
+	def article_cover url, options={}
+		html_class = options[:class]
+		html_style = "background:url(#{url});background-size:cover;"
+		html = "<header style='#{html_style}' class='#{html_class}'></header>"
+		html.html_safe
+	end
+	
 end
